a436e65b7238a1da24543b1730f4df2010debe38
brittrestine <brittrestine@gmail.com>
2020-09-19 16:30:06 -0500

    create table

--- src/list/index.js
+++ src/list/index.js
@@ -1,4 +1,4 @@
-// import '../App.css'
+import '../App.css'
 
 import React, { useEffect, useState } from 'react'
 
@@ -6,7 +6,6 @@ import _ from 'lodash'
 import {GITHUB_URL as gitHubUrl} from '../constants'
 
 function List() {
-
   const [list, setList] = useState();
 
   useEffect(()  => {
@@ -14,70 +13,53 @@ function List() {
   }, []);
 
   const getFetchRewardsApi = async () => {
-      // await fetch('https://fetch-hiring.s3.amazonaws.com/hiring.json', {mode: 'no-cors', headers: {'Access-Control-Allow-Origin': '*'}})
-      // await fetch(gitHubUrl, {mode: 'no-cors', headers: {'Access-Control-Allow-Origin': '*'}})
-      // await fetch(gitHubUrl, {mode: 'no-cors', headers: {'Access-Control-Allow-Origin': '*'}})
-      // .then(response => response.text())
-
-      // await fetch('https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/posts.json')
-      await fetch('http://dummy.restapiexample.com/api/v1/employees')
-      .then(response => response.json())
-      .then(data => sortData(data))
-      .catch(error => console.log('Request failed', error))
-
+    await fetch('https://fetch-hiring.s3.amazonaws.com/hiring.json')
+    .then(response => response.json())
+    .then(data => sortData(data))
+    .catch(error => console.log('Request failed', error))
   }
 
   const sortData = (data) => {
     let ordered = []
 
-    const grouped = _.groupBy(data.data, function(list) {
-      return list.employee_age;
+    //groupBy listId first
+    const grouped = _.groupBy(data, function(list) {
+      return list.listId;
     })
-
-    // groupBy listId first
-    // const grouped = _.groupBy(data, function(list) {
-    //   return list.listId;
-    // })
 
     // then sortby listId then name
     Object.values(grouped).map((group) => {
-        const sortedby = (_.sortBy(group, list => [list.id, list.employee_name], ['asc', 'asc']))
-        // const sortedby = (_.sortBy(group, list => [list.listId, list.name], ['asc', 'asc']))
+        const sortedby = (_.sortBy(group, list => [list.listId, list.name], ['asc', 'asc']))
         sortedby.map((i) => ordered.push(i))
     })
 
     setList(ordered)
   }
 
-
-  console.log("list...", list )
-  console.log("list...", !!list )
-
   return (
     <div>
-      <table>
+      <table className="table">
+        <thead className="table-header">
+          <tr>
+            <th>Id</th>
+            <th>List Id</th>
+            <th>Name</th>
+          </tr>
+        </thead>
 
-      <thead>
-        <tr>
-          <th>List Id</th>
-          <th>Name</th>
-        </tr>
-      </thead>
-
-        {!!list &&
-          list.map((item) => {
-            return (
-              <>
-              <tbody>
-                <tr key={item.id}>
-                 <td>{item.employee_name}</td>
-                 <td>{item.employee_age}</td>
+        <tbody c>
+          {!!list &&
+            list.map((item) => {
+              return (
+                <tr className="table-body" key={item.id}>
+                 <td>{item.id}</td>
+                 <td>{item.listId}</td>
+                 <td>{item.name}</td>
                 </tr>
-              </tbody>
-             </>
-            )
-          })
-        }
+              )
+            })
+          }
+        </tbody>
       </table>
     </div>
   )
